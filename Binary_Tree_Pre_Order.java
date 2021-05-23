3a8d62c772d69e33dafbc24eac79c13ddce71c2a
Anchit Aggarwal <anchitaggarwal506@gmail.com>
2021-05-23 14:24:15 +0530

    Create Binary_Tree_Pre_Order.java
    Given the root of a binary tree, return the preorder traversal of its nodes' values.
    Input: root = [1,null,2,3]
    Output: [1,2,3]

---
+++ Binary_Tree_Pre_Order.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list=new ArrayList<>();
+        if(root==null){
+            return list;
+        }
+        Stack<TreeNode> stack=new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode temp=stack.pop();
+            list.add(temp.val);
+            if(temp.right!=null){
+                stack.push(temp.right);
+            }
+            if(temp.left!=null){
+                stack.push(temp.left);
+            }
+        }
+        
+        return list;
+    }
+}
